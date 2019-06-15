4d68ce2a7f291331c9f9f48a34dc02f4fbf9c450
Kyle Air <20861192+cpkm@users.noreply.github.com>
2019-06-14 22:12:21 -0400

    update admin user

--- dartssite.py
+++ dartssite.py
@@ -1,9 +1,10 @@
 from app import create_app, db
-from app.models import Player, Game, Match, PlayerGame, Team, Season, PlayerSeasonStats, MatchStats
+from app.models import Player, Game, Match, PlayerGame, Team, Season, PlayerSeasonStats, MatchStats, User
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Player': Player, 'Game': Game, 'Match': Match, 
-        'PlayerGame': PlayerGame, 'Team': Team, 'Season': Season, 'PlayerSeasonStats': PlayerSeasonStats}
+        'PlayerGame': PlayerGame, 'Team': Team, 'Season': Season, 
+        'PlayerSeasonStats': PlayerSeasonStats, 'User': User}
