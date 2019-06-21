15bd4c4b58f45317beeb737a63e0a3d4ceeb74e2
Kyle Air <20861192+cpkm@users.noreply.github.com>
2019-06-21 13:43:23 -0400

    creates routing decorator for roles

--- app/decorators.py
+++ app/decorators.py
@@ -11,5 +11,16 @@ def check_verification(func):
             flash('Please verify your account!', 'warning')
             return redirect(url_for('auth.unverified_email'))
         return func(*args, **kwargs)
-
     return decorated_function
+
+
+def check_role(roles):
+    def check_role_decorator(func):
+        @wraps(func)
+        def decorated_function(*args, **kwargs):
+            if not current_user.role in roles:
+                flash('You do not have permission to view this page.', 'warning')
+                return redirect(url_for('main.index'))
+            return func(*args, **kwargs)
+        return decorated_function
+    return check_role_decorator
