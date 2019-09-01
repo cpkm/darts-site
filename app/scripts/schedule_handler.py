90499c9c0cf7670576b43a416b6812589d6e6bf1
Ryan Dunlop <rdunlop@uoguelph.ca>
2019-09-01 10:59:45 -0400

    Added some formatting checking for playoffs

--- app/scripts/schedule_handler.py
+++ app/scripts/schedule_handler.py
@@ -6,11 +6,11 @@ import pandas as pd
 # 08/02/2019, 'Fat Duck 2', 'A', 'n'
 
 #This allows playoffs to be set as y/n, p/r or 1/0
-
+DEBUG = 1
 
 def FormatColumnNames(df):
     #Makes sure the title names are what is expected
-    df.columns = df.columns.str.title()
+    df.columns = df.columns.str.title().str.strip(' ')
     return df
 
 def FormatDate(dates):
@@ -25,31 +25,50 @@ def FormatLocation(locations):
 
 def FormatPlayoff(playoffs):
     #Turns loose playoff formatting into a boolean column
-    mapping = {'y': True, 'n': False, 'r': False, 'p': True }
+    mapping = { 'y': True, 'n': False, 'r' : False, 'p' : True }
     if playoffs.dtype != 'bool':
-        playoffs.map(mapping)
+        playoffs = playoffs.str.lower()
+        playoffs = playoffs.str.strip(' ')
+        playoffs = playoffs.str.strip("'")
+        playoffs = playoffs.str.strip('"')
+        if DEBUG:
+            print('After FormatPlayoff...')
+            print(playoffs.head())
+        playoffs = playoffs.map(mapping)
         playoffs = playoffs.astype('bool')
+        if DEBUG:
+            print('After mapping...')
+            print(playoffs.head())
     return playoffs
 
 def FormatEntries(df):
     df.Date = FormatDate(df.Date)
     df.Team = FormatTeam(df.Team)
-    df.Location = FromatLocation(df.Location)
+    df.Location = FormatLocation(df.Location)
     df.Playoff = FormatPlayoff(df.Playoff)
     return df
 
 def CheckColumns(df):
     #Checks that all of the columns that are needed exist
     needed_columns = ['Date', 'Team', 'Location', 'Playoff']
-    missing_lst = [ missing for missing in needed_columns if not missing.issubset(df.columns) ]
-    if(len(missing_lst))
+    missing_lst = [ missing for missing in needed_columns if missing not in df.columns ]
+    if len(missing_lst) > 0:
         raise ValueError('Missing columns: {}'.format(missing_lst))
 
 def read_csv(filename):
     #Reads the schedule from a csv
     df_sched = pd.read_csv(filename)
+    if DEBUG:
+        print('After Reading...')
+        print(df_sched.head())
     df_sched = FormatColumnNames(df_sched)
+    if DEBUG:
+        print('After FormatColumnNames...')
+        print(df_sched.head())
     CheckColumns(df_sched)
     df_sched = FormatEntries(df_sched)
+    if DEBUG:
+        print('After FormatEntries...')
+        print(df_sched.head())
     return df_sched
 
