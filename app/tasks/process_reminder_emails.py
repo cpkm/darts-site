bebda40d8da7a204ba3328247b964735e6ff26ff
Kyle Air <20861192+cpkm@users.noreply.github.com>
2019-09-19 10:05:12 -0400

    send reminder emails

--- app/tasks/process_reminder_emails.py
+++ app/tasks/process_reminder_emails.py
@@ -7,14 +7,15 @@ from app.main.email import send_reminder_email
 def find_reminder_matches(category):
 
     reminders = ReminderSettings.query.filter_by(category=category).all()
-
+    print(reminders)
+    matches = []
     for r in reminders:
         dia = date.today() + timedelta(r.days_in_advance)
         matches = Match.query.filter(Match.date >= date.today(), Match.date <= dia).all()
 
         for m in matches:
             if m.reminder_email_sent:
-                if (m.date-m.reminder_email_sent) <= r.days_in_advance:
+                if (m.date-m.reminder_email_sent) <= timedelta(r.days_in_advance):
                     matches.remove(m)
 
     return matches
@@ -22,20 +23,19 @@ def find_reminder_matches(category):
 
 
 app = create_app()
-with app.app_context():
+with app.app_context(), app.test_request_context():
 
     matches = find_reminder_matches('match reminder')
+    print(matches)
 
     for m in matches:
-        users = [p.user for p in current_roster() if p.user is not None]
-        status = [u.player.checked_matches_association.filter_by(match_id=match.id).first().status for u in users]
-        
-        send_reminder_email(users=users,match=m,status=status)
-        match.reminder_email_sent = date.today()
-        db.session.add(match)
-        db.session.commit()
-        #send reminder email
-
-    print('Done!')
-
-
+        try:
+            users = [p.user for p in current_roster() if p.user is not None]
+            status = [u.player.checked_matches_association.filter_by(match_id=m.id).first().status for u in users]
+            
+            send_reminder_email(users=users,match=m,status=status)
+            m.reminder_email_sent = date.today()
+            db.session.add(m)
+            db.session.commit()
+        except Exception as e:
+            print('Error sending reminder:', e)
