07faa2a5841ecaca54ecd6283d22affb6c94ff76
Kyle Air <20861192+cpkm@users.noreply.github.com>
2019-09-24 09:45:01 -0400

    removes old columns from User and Player

--- app/models.py
+++ app/models.py
@@ -18,7 +18,6 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    role = db.Column(db.String(32), index=True, default='player')
     registered_on = db.Column(db.Date, index=True, default=date.today())
     verified = db.Column(db.Boolean, index=True, default=False)
     verified_on = db.Column(db.Date, index=True, default=None)
