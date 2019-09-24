a50e6f8cd4747dde91141b6ce3498d9160fe84bb
Kyle Air <20861192+cpkm@users.noreply.github.com>
2019-09-24 15:06:21 -0400

    add mail suppression

--- config.py
+++ config.py
@@ -17,6 +17,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SUPPRESS_SEND = os.environ.get('MAIL_SUPPRESS_SEND') is not None
 
     ADMINS = [('ICC4 darts','icc4darts@gmail.com')]
     UPLOADS_DEFAULT_DEST = os.environ.get('UPLOAD_FOLDER') or 'app/var/tmp/uploads'
