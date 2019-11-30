541a4b3010c198db3476c15731196b04cccaa2e6
Kyle Air <20861192+cpkm@users.noreply.github.com>
2019-11-30 16:18:38 -0500

    fixes boolean error with checkin anonymous check

--- app/main/routes.py
+++ app/main/routes.py
@@ -731,10 +731,8 @@ def update_checkin(token):
     player.checkin(match,status)
 
     flash('Thank you for checking in!', 'success')
-    if current_user.is_anonymous():
+    if current_user.is_anonymous:
         return redirect(url_for('main.index'))
-    if current_user.check_role(['captain','admin']):
-        return redirect(url_for('main.captain', _anchor='checkin'))
     elif current_user == user:
         return redirect(url_for('main.checkin'))
     else:
